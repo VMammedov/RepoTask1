e26d9f262bcb551a4c6fd2cc1831077e8431c28a
VMammedov <vusal.memmedli25official@gmail.com>
2021-06-25 11:49:37 +0400

    First Commit

---
+++ Project1/Department.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Department
+    {
+        private string name;
+        private int workerLimit;
+        private int salaryLimit;
+        public List<Employee> Employees = new List<Employee>();
+
+        public int SalaryLimit
+        {
+            get { return salaryLimit; }
+            set { salaryLimit = value; }
+        }
+
+        public int WorkerLimit
+        {
+            get { return workerLimit; }
+            set { workerLimit = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double CalcSalaryAverage()
+        {
+            int sum = 0;
+            foreach (Employee employer in Employees)
+            {
+                sum += employer.Salary;
+            }
+            return (double)sum / Employees.Count;
+        }
+    }
+}
---
+++ Project1/IHumanResourceManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public interface IHumanResourceManager
+    {
+        List<Department> Departments { get; set; }
+        void AddDepartment(string name, int workerlimit, int salarylimit);
+        List<Department> GetDepartments();
+        void EditDepartments(string DName, string newname);
+        void AddEmployee(string fullname, string position, int salary, string DName);
+        void RemoveEmployee(string no, string DName);
+        void EditEmployee(string no, int salary, string position);
+    }
+}
